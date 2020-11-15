5f411a79f8c72d2ca54601e937f813708e703da3
Brian Mowrey <brian@drlabs.org>
2020-11-15 13:29:06 -0600

    * added import script * added test learning spark, used black and white for svg instead of not showing text * bad decoding in html learning spark so not sure that's the best choice (lose CRs anyway)

---
+++ _posts/2020-11-14-learning-spark.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "Learning Spark"
+book: learning-spark
+author: Holden Karau, Andy Konwinski, Patrick Wendell, Matei Zaharia
+kindle: false
+date: 2020-11-14
+---
---
+++ import/import_book.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import fire
+from pathlib import Path
+import os
+import sys
+from bs4 import BeautifulSoup
+import yaml
+
+class Highlights(object):
+    """converts book highlights"""
+
+    def html_to_yaml(self, html_file):
+        """
+        converts html file to yaml (for highlights jekyll site)
+        """
+        html_path = Path(html_file)
+        yaml_path = html_path.parent / '../../_data/' / (str(html_path.stem) + '.yml')
+        yaml_path = yaml_path.resolve()
+        highlights = []
+        print(f"yaml_path={yaml_path}")
+        if not html_path.exists():
+            print(f"{html_file} does not exist")
+            return
+
+        with html_path.open() as f:
+            soup = BeautifulSoup(f, 'html.parser')
+        
+        #print(f"soup={soup}")
+        for annotation in soup.find_all(name="div", class_='3D"annotation"'):
+            print(f"annotation={annotation}")
+            chapter = annotation.find(name='div', class_='3D"annotationchapter"').get_text().lstrip()
+            print(f"chapter: {chapter}")
+            text = annotation.find(name='p', class_='3D"annotationrepresentativetext"').get_text().lstrip()
+            text = text.replace('3D', '') # why are these here?
+            print(f"text: {text}")
+            highlights.append({'text': text, 'page': chapter})
+
+        with yaml_path.open(mode='w') as f:
+            yaml.dump(highlights, f)
+
+        print(f"wrote {len(highlights)} entries")
+        # TODO: create 
+
+
+if __name__ == '__main__':
+    fire.Fire(Highlights)
+
+
